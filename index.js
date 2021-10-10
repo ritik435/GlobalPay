6e75665ca70c000427111f59aa5ffda8389145dd
Ritik <ritik12022001@gmail.com>
2021-10-10 17:20:27 +0530

    added sass-middleware -->passbook,signIn,header

--- index.js
+++ index.js
@@ -9,7 +9,16 @@ const session=require('express-session');
 const passport=require('passport');
 const passportLocal=require('./config/passport-local-strategy');
 const MongoStore = require('connect-mongo');
+const sassMiddleware= require('node-sass-middleware');
 
+//sass-middleware
+app.use(sassMiddleware({
+    src: './assets/scss',
+    dest: './assets/css',
+    debug: true,
+    outputStyle: 'extended',
+    prefix: '/css'
+}))
 
 //setup the ejs-layouts
 app.use(ejsLayouts);
